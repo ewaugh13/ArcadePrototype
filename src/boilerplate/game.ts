e41cf005972d09ff2f59e16779cef1bd6faabdbb
Vikram Venkatesh <venkateshvikram96@gmail.com>
2019-09-08 18:01:23 -0600

    Added platform gen

--- src/boilerplate/game.ts
+++ src/boilerplate/game.ts
@@ -7,8 +7,6 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 };
 
 export class GameScene extends Phaser.Scene {
-  //private square: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
-  private player;
   constructor() {
     super(sceneConfig);
   }
@@ -16,21 +14,53 @@ export class GameScene extends Phaser.Scene {
   public preload(){
     
     this.load.image('background','src/boilerplate/assets/Ship.png');
-    this.load.spritesheet('player', 
-    'src/boilerplate/assets/scottpilgrim_multiple.png',
+    this.load.spritesheet('dude', 
+    'src/boilerplate/assets/dude.png',
     { frameWidth: 128, frameHeight: 128 });
     this.load.image('platformPlank','src/boilerplate/assets/plank.png');
+    
   }
+
   public create() {
+    var platforms;
+    var player;
     this.add.image(0,-3240,'background').setOrigin(0,0);
+    platforms = this.physics.add.staticGroup()
+    
+    //Platform Generation
     var i;
-    var j;
+    var j; 
     for(i=0; i<10 ; i++){
-     var seed = 400+Math.random()*1000;
+     var seed = 353+Math.random()*1213;  //left wall: 353, width till right wall: 1213(1566-352)
       for(j=0; j<7; j++){
-       this.add.image(seed+(j*32),32+(i*100),'platformPlank');
+        platforms.create(seed+(j*32),32+(i*100),'platformPlank'); 
       }
     }
+
+    //Player Creation
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
   }
  
   public update() {
@@ -67,7 +97,8 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
   physics: {
     default: 'arcade',
     arcade: {
-      debug: true,
+      gravity: {y: 300},
+      debug: false,
     },
   },
   scene : GameScene,
