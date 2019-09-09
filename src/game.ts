d0e65ebbe887b6d531d1df2f21551a50d09d67de
Vikram Venkatesh <venkateshvikram96@gmail.com>
2019-09-08 19:06:03 -0600

    Added Gems

--- src/game.ts
+++ src/game.ts
@@ -8,8 +8,9 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 export class GameScene extends Phaser.Scene {
   
-  private player;
-  
+  private player: Phaser.Physics.Arcade.Sprite;
+  private platforms: Phaser.Physics.Arcade.StaticGroup;
+  private gems: Phaser.GameObjects.Group;
   constructor() {
     super(sceneConfig);
   }
@@ -21,35 +22,42 @@ export class GameScene extends Phaser.Scene {
     'src/assets/dude.png',
     { frameWidth: 32, frameHeight: 48 });
     this.load.image('platformPlank','src/assets/plank.png');
-    //this.load
+    this.load.image('gem','src/assets/Gem.png');
   }
 
   public create() {
-    var platforms;
-    //var player;
+
     this.add.image(0,-3240,'background').setOrigin(0,0);
-    platforms = this.physics.add.staticGroup()
+    this.platforms = this.physics.add.staticGroup()
     
     //Platform Generation
-    var i;
-    var j; 
-    var k;
+    var i: number;
+    var j: number; 
+    var k: number;
     for(i=0; i<10 ; i++){
      var seed = 353+Math.random()*1213;  //left wall: 353, width till right wall: 1213(1566-352)
       for(j=0; j<7; j++){
         if(i==9){
           for(k=0; k<37; k++)
-            platforms.create(353+(k*32) ,32+(i*100),'platformPlank')
+            this.platforms.create(353+(k*32) ,32+(i*100),'platformPlank')
         }
         else
-          platforms.create(seed+(j*32),32+(i*100),'platformPlank'); 
+          this.platforms.create(seed+(j*32),32+(i*100),'platformPlank'); 
       }
     }
+    
+    this.gems = this.physics.add.group({
+      key: 'gem',
+      repeat: 11,
+      setXY: { x: 502, y: 400, stepX: 70 }
+  });
+  
+  this.physics.add.collider(this.gems, this.platforms);
 
     //Player Creation
     this.player = this.physics.add.sprite(500, 150, 'dude');
     this.player.setCollideWorldBounds(true);
-    this.physics.add.collider(this.player, platforms);
+    this.physics.add.collider(this.player, this.platforms);
   
     this.anims.create({
         key: 'left',
@@ -73,33 +81,38 @@ export class GameScene extends Phaser.Scene {
   }
  
   public update() {
-    const cursors = this.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown)
-{
-    this.player.setVelocityX(-160);
+      this.physics.add.overlap(this.player, this.gems, collectStar, null, this);
+      const cursors = this.input.keyboard.createCursorKeys();
+      if (cursors.left.isDown)
+    {
+      this.player.setVelocityX(-160);
 
-    this.player.anims.play('left', true);
-}
-else if (cursors.right.isDown)
-{
-    this.player.setVelocityX(160);
+      this.player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+      this.player.setVelocityX(160);
 
-    this.player.anims.play('right', true);
-}
-else
-{
-    this.player.setVelocityX(0);
+      this.player.anims.play('right', true);
+    }
+    else
+    {
+      this.player.setVelocityX(0);
 
-    this.player.anims.play('turn');
-}
+      this.player.anims.play('turn');
+    }
 
-if (cursors.up.isDown && this.player.body.touching.down)
-{
-    this.player.setVelocityY(-330);
-}  
+    if (cursors.up.isDown && this.player.body.touching.down)
+    {
+      this.player.setVelocityY(-330);
+    }  
   
   }
   
+}
+
+function collectStar(player,gem) {
+    gem.disableBody(true,true);
 }
 
 const gameConfig: Phaser.Types.Core.GameConfig = {
